635a58f53014fe0d17952e879dba8167bd08df23
KwangZung <162069078+KwangZung@users.noreply.github.com>
2024-03-15 22:24:25 +0700

    Create fallingRock.cpp

---
+++ fallingRock.cpp
@@ -0,0 +1,30 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    int w, h;
+    cin >> w >> h;
+    
+    char matrix [w][h];
+    
+    for (int i=0; i<w; i++) {
+        for (int j=0; j<h; j++) cin >> matrix[i][j];
+    }
+    
+    int pos;
+    for (int i=0; i<w; i++) if (matrix[1][])
+    
+    bool can_escapse;
+    
+    for (int t=0; t<h; t++) {
+        
+    }
+    
+    return 0;
+}
