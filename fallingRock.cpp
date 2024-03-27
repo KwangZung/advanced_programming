c71f806095374d5c73bff6685d2d33eba707fcf5
KwangZung <162069078+KwangZung@users.noreply.github.com>
2024-03-27 22:01:03 +0700

    Update fallingRock.cpp

--- fallingRock.cpp
+++ fallingRock.cpp
@@ -5,26 +5,46 @@
 #include <algorithm>
 using namespace std;
 
+int w, h;
+
+vector<vector<char>> grid(2000, vector<char>(2000));
+
+bool canSurvive (vector <vector<char>> grid, int x, int y)
+{
+    if (y==h-1) return true;
+    
+    bool canLeft = (x>0 && grid[y][x-1]=='E' && grid[y+1][x-1]=='E');
+    bool canRight = (x<w-1 && grid[y][x+1]=='E' && grid[y+1][x+1]=='E');
+    bool canStay = (grid [y+1][x]=='E');
+    
+    //if (!canLeft && !canRight && !canStay) return false;
+    
+    if ( (canLeft&&canSurvive(grid, x-1, y+1)) || (canRight&&canSurvive(grid, x+1, y+1)) || (canStay&&canSurvive(grid, x, y+1))) return true;
+    
+    return false;
+}
+
 
 int main() {
     /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
-    int w, h;
+    
     cin >> w >> h;
     
-    char matrix [w][h];
+    int x, y;
     
-    for (int i=0; i<w; i++) {
-        for (int j=0; j<h; j++) cin >> matrix[i][j];
+    
+    for (int i = 0; i < h; i++) {
+        for (int j = 0; j < w; j++) {
+            cin >> grid[i][j];
+            if (grid[i][j]=='Y') 
+            {
+                x=j;
+                y=i;
+            }
+        }
     }
     
-    int pos;
-    for (int i=0; i<w; i++) if (matrix[1][])
+    if (canSurvive(grid, x, y)) cout << "YES";
+    else cout << "NO";
     
-    bool can_escapse;
-    
-    for (int t=0; t<h; t++) {
-        
-    }
-    
-    return 0;
 }
