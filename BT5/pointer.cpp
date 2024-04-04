96869b4e8b4fb35cb3d5633fea81ea93d32ccac8
Nguyễn Quang Dũng <162069078+KwangZung@users.noreply.github.com>
2024-04-04 13:22:48 +0700

    Create pointer.cpp

---
+++ BT5/pointer.cpp
@@ -0,0 +1,26 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+void update (int *a, int *b)
+{
+    int tmp = *a;
+    *a = *a+*b;
+    *b = abs(*b-tmp);
+}
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    int a,b;
+    cin >> a >> b;
+    
+    update (&a,&b);
+    
+    cout << a << "\n" << b;
+       
+    return 0;
+}
