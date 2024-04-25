b2c8e96dab49d987d87ca7110f99ab9a66785eba
Nguyễn Quang Dũng <162069078+KwangZung@users.noreply.github.com>
2024-04-25 11:29:37 +0700

    Create structs.cpp

---
+++ BT6/structs.cpp
@@ -0,0 +1,24 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+struct student 
+{
+    int age;
+    string first_name;
+    string last_name;
+    int standard;
+     
+};
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */ 
+    student a;
+    cin >> a.age >> a.first_name >> a.last_name >> a.standard;
+    cout << a.age << " " << a.first_name << " " << a.last_name << " " << a.standard;  
+    return 0;
+}
